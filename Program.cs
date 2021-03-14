7ee71ff00cef021da8053c90acfd5a36193b0642
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-03-14 15:59:29 -0300

    Finaliza programa Tradutor

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetSpeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("|Digite uma frase para conversão|");
+            Console.WriteLine("---------------------------------");
+            Console.ResetColor();
+            Console.WriteLine();
+            string TextoUsuario =  Console.ReadLine();
+            string NovaFrase =  TextoUsuario.Replace("A","4").Replace("E","3").Replace("I","1").Replace("O","0").Replace("S","5").Replace("T","7").Replace("a","4").Replace("e","3").Replace("i","1").Replace("o","0").Replace("s","5").Replace("t","7");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;       
+            Console.WriteLine(NovaFrase);
+            Console.ResetColor();
+            
+        }
+    }
+}
